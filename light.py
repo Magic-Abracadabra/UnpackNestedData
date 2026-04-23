d9cd4c5802ad7ad1c5f0ed16108ea39ad91913f5
自有计算（Self-Sovereign Computing） <196052567+Magic-Abracadabra@users.noreply.github.com>
2026-04-23 20:37:51 +0800

    Update light.py

--- light.py
+++ light.py
@@ -1,4 +1,4 @@
-def light(key, d) -> (bool, list):
+def light(key, d) -> tuple[bool, list]:
 	'''Returning (l, values). If the *key* is in the nested data d, then l=True. The list *values* will be composed of all iterable objects containing the key in d.'''
 	l = False
 	values = []
@@ -9,7 +9,7 @@ def light(key, d) -> (bool, list):
 		if isinstance(d, dict):
 			if key in d.values():
 				l = True
-				if d[-1] != d:
+				if not values or values[-1] != d:
 					values.append(d)
 			for x in d.values():
 				if hasattr(x, '__iter__') and not isinstance(x, str):
