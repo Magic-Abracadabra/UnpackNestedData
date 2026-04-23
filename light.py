a77fc07a7b926629ed40b55c8f09414531195cbc
自有计算（Self-Sovereign Computing） <196052567+Magic-Abracadabra@users.noreply.github.com>
2026-04-23 20:14:21 +0800

    Update light.py

--- light.py
+++ light.py
@@ -11,10 +11,15 @@ def light(key, d) -> (bool, list):
 				l = True
 				if d[-1] != d:
 					values.append(d)
-			d = d.values()
-		for x in d:
-			if hasattr(x, '__iter__') and not isinstance(x, str):
-				L, value = light(key, x)
-				l = any([l, L])
-				values += value
+			for x in d.values():
+				if hasattr(x, '__iter__') and not isinstance(x, str):
+					L, value = light(key, x)
+					l = any([l, L])
+					values += value
+		else:
+			for x in d:
+				if hasattr(x, '__iter__') and not isinstance(x, str):
+					L, value = light(key, x)
+					l = any([l, L])
+					values += value
 	return l, values
