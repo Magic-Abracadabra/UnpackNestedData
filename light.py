777f2c43d0eafac6b90684c5b0fb1cec810b3a3f
自有计算（Self-Sovereign Computing） <196052567+Magic-Abracadabra@users.noreply.github.com>
2026-04-23 19:32:49 +0800

    Update light.py

--- light.py
+++ light.py
@@ -2,22 +2,26 @@ def light(key, d) -> (bool, list):
 	'''Returning (l, values). If the *key* is in the nested data d, then l=True. The list *values* will be composed of all iterable objects containing the key in d.'''
 	l = False
 	values = []
-	if key in d:
-		l = True
-		values.append(d)
 	try:
 		iter(d)
+		if key in d:
+			l = True
+			values.append(d)
 		if type(d) == dict:
 			d = d.items()
 		for x in d:
-			if key in x:
-				l = True
-				values.append(x)
-			else:
-				l_, value = light(key, x)
-				l = any([l, l_])
-				if l_:
-					values += value
+			try:
+				iter(x)
+				if key in x:
+					l = True
+					values.append(x)
+				else:
+					l_, value = light(key, x)
+					l = any([l, l_])
+					if l_:
+						values += value
+			except:
+				pass
 		return l, values
 	except:
 		return l, values
